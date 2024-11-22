6547bbc450b358612f20dec518201cf19e431fdb
Pedro Mateus <pedropmateus91@gmail.com>
2024-11-22 16:45:04 +0000

    Register user

---
+++ Dotnet8JwtApi/Migrations/20241122164154_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Dotnet8JwtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "20f3a69d-7be7-43b4-859c-b12e380d5c0e", null, "Admin", "ADMIN" },
+                    { "6f150d0d-95b0-4021-9e25-b1595e2816b0", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "20f3a69d-7be7-43b4-859c-b12e380d5c0e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6f150d0d-95b0-4021-9e25-b1595e2816b0");
+        }
+    }
+}
